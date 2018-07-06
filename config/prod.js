9a01344c24a2f06913d116cf2f0410e2a80a5f87
Matthew Keller <matthewkeller@Hews-MacBook-Air.local>
2018-07-06 11:25:33 -0400

    added poduction keys

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,14 @@
-module.exports = {
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY
-  };
+// module.exports = {
+//     googleClientID: process.env.GOOGLE_CLIENT_ID,
+//     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     mongoURI: process.env.MONGO_URI,
+//     cookieKey: process.env.COOKIE_KEY
+//   };
+
+
+  module.exports = {
+    googleClientID: '161837321482-ptc9r5lpqhr8o60t95gc5fhrs3jrmj4c.apps.googleusercontent.com',
+    googleClientSecret: 'BZmZYel3NJfcRKXuz8agINws',
+    mongoURI: 'mongodb://localhost/pachira-financial',
+    cookieKey: 'usr'
+  }
