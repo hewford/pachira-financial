7397eedccbf70cc287fa260cab70a1e1bc444f8e
Matthew Keller <matthewkeller@Hews-MacBook-Air.local>
2018-07-21 18:26:21 -0400

    changed ndex.js to join paths

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ const keys = require('./config/keys')
 const bodyParser = require('body-parser');
 const cors = require("cors");
 const path = require('path');
+
 require('./services/passport')
 console.log('loaded')
+
+console.log('=========')
+console.log('INDEX.JS HIT')
+console.log('=========')
 
 mongoose.connect(keys.mongoURI)
 
 const app = express()
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+  }
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -29,18 +37,18 @@ app.use(passport.session())
 require('./routes/authRoutes')(app)
 require('./routes/planRoutes')(app)
 
-if (process.env.NODE_ENV === 'production') {
-    // Express will serve up production assets
-    // like our main.js file, or main.css file!
-    app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//     // Express will serve up production assets
+//     // like our main.js file, or main.css file!
+//     app.use(express.static('client/build'));
   
-    // Express will serve up the index.html file
-    // if it doesn't recognize the route
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+//     // Express will serve up the index.html file
+//     // if it doesn't recognize the route
+//     const path = require('path');
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     });
+//   }
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
