dbcdeef669a452d8700fb9804b4650c8d4c5af06
Matthew Keller <matthewkeller@Hews-MacBook-Air.local>
2018-07-22 13:07:23 -0400

    n

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const app = express()
 if (process.env.NODE_ENV === 'production') {
     // Express will serve up production assets
     // like our main.js file, or main.css file!
-    app.use(express.static('client/build'));
+    app.use(express.static('/client/build'));
   
     // Express will serve up the index.html file
     // if it doesn't recognize the route
